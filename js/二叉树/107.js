0a56cc57863e40f87dac06d9c65ead46fa51e8c4
Mxz <954380531@qq.com>
2020-12-23 09:57:40 +0800

    add 107

---
+++ js/二叉树/107.js
@@ -0,0 +1,53 @@
+// 107. 二叉树的层序遍历 II
+// 给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+// 例如：
+// 给定二叉树 [3,9,20,null,null,15,7],
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// 返回其自底向上的层序遍历为：
+
+// [
+//   [15,7],
+//   [9,20],
+//   [3]
+// ]
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  if (!root) return []
+  let queue = [root]
+  let res = []
+  while (queue.length) {
+    let len = queue.length
+    let subArr = []
+    for (let i = 0; i < len; i++) {
+      let cur = queue.shift()
+      subArr.push(cur.val)
+      if (cur.left) {
+        queue.push(cur.left)
+      }
+      if (cur.right) {
+        queue.push(cur.right)
+      }
+    }
+    res.push(subArr)
+  }
+  return res.reverse()
+};
+
+// 思路见 #102
