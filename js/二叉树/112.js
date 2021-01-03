51e0525965660eae86acb9456a361fcf10a1db2d
Mxz <954380531@qq.com>
2021-01-03 13:13:19 +0800

    add 112

---
+++ js/二叉树/112.js
@@ -0,0 +1,42 @@
+// 112. 路径总和
+// 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+// 说明: 叶子节点是指没有子节点的节点。
+
+// 示例: 
+// 给定如下二叉树，以及目标和 sum = 22，
+
+//               5
+//              / \
+//             4   8
+//            /   / \
+//           11  13  4
+//          /  \      \
+//         7    2      1
+// 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+  if (!root) {
+    return false
+  }
+  // 叶子节点 并且值相等时才是 true
+  if (!root.left && !root.right && root.val === sum) {
+    return true
+  }
+  // 每次递归时 sum 减去当前节点值
+  let resLeft = hasPathSum(root.left, sum - root.val)
+  let resRight = hasPathSum(root.right, sum - root.val)
+  return resLeft || resRight
+};
