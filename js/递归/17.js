09c50bee5df4d7b16ccd4fdff0505a6583db8838
Mxz <954380531@qq.com>
2021-01-04 10:40:03 +0800

    add 17

---
+++ js/递归/17.js
@@ -0,0 +1,49 @@
+// 17. 电话号码的字母组合
+// 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+
+
+// 示例:
+
+// 输入："23"
+// 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+// 说明:
+// 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterMap = {
+  '0': ' ',
+  '1': '',
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz',
+}
+var letterCombinations = function(digits) {
+  let res = []
+  if (!digits) {
+    return res
+  }
+  // digits 要处理的数组 如 '23' index 当前处理的索引 s 当前拼接的字符串
+  var findCombinations = function(digits, index, s) {
+    if (index === digits.length) {
+      res.push(s)
+      return
+    }
+    const c = letterMap[digits[index]] // abc
+    for (let char of c) {
+      findCombinations(digits, index + 1, s + char)
+    }
+  }
+  findCombinations(digits, 0, '')
+  return res
+};
