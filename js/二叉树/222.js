a91b2706e223c0f20d33b33ea04b839ba39785b2
Mxz <954380531@qq.com>
2021-01-18 11:41:45 +0800

    add 222's comments

--- js/二叉树/222.js
+++ js/二叉树/222.js
@@ -57,9 +57,9 @@ var countNodes = function(root) {
   const left = getDepth(root.left)
   const right = getDepth(root.right)
   if (left === right) {
-    return countNodes(root.right) + (1 << left)
+    return countNodes(root.right) + (1 << left) // 1(根节点) + (1 << ld)-1(左完全左子树节点数) + 右子树节点数量
   } else {
-    return countNodes(root.left) + (1 << right)
+    return countNodes(root.left) + (1 << right) // 1(根节点) + (1 << rd)-1(右完全右子树节点数) + 左子树节点数量
   }
 };
 var getDepth = (root) => {
@@ -70,3 +70,10 @@ var getDepth = (root) => {
   }
   return depth
 }
+
+/**
+完全二叉树的高度可以直接通过不断地访问左子树就可以获取
+判断左右子树的高度: 
+如果相等说明左子树是满二叉树, 然后进一步判断右子树的节点数(最后一层最后出现的节点必然在右子树中)
+如果不等说明右子树是深度小于左子树的满二叉树, 然后进一步判断左子树的节点数(最后一层最后出现的节点必然在左子树中)
+**/
