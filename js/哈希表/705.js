6553e330a8c1647b136b9b5d6243b8b37ebf36e0
Mxz <954380531@qq.com>
2021-03-13 10:08:06 +0800

    add 705

---
+++ js/哈希表/705.js
@@ -0,0 +1,77 @@
+// 705. 设计哈希集合
+// 不使用任何内建的哈希表库设计一个哈希集合（HashSet）。
+
+// 实现 MyHashSet 类：
+
+// void add(key) 向哈希集合中插入值 key 。
+// bool contains(key) 返回哈希集合中是否存在这个值 key 。
+// void remove(key) 将给定值 key 从哈希集合中删除。如果哈希集合中没有这个值，什么也不做。
+ 
+// 示例：
+
+// 输入：
+// ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+// [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+// 输出：
+// [null, null, null, true, false, null, true, null, false]
+
+// 解释：
+// MyHashSet myHashSet = new MyHashSet();
+// myHashSet.add(1);      // set = [1]
+// myHashSet.add(2);      // set = [1, 2]
+// myHashSet.contains(1); // 返回 True
+// myHashSet.contains(3); // 返回 False ，（未找到）
+// myHashSet.add(2);      // set = [1, 2]
+// myHashSet.contains(2); // 返回 True
+// myHashSet.remove(2);   // set = [1]
+// myHashSet.contains(2); // 返回 False ，（已移除）
+ 
+
+// 提示：
+
+// 0 <= key <= 106
+// 最多调用 104 次 add、remove 和 contains 。
+ 
+
+// 进阶：你可以不使用内建的哈希集合库解决此问题吗？
+
+/**
+ * Initialize your data structure here.
+ */
+ var MyHashSet = function() {
+  this.hash = {}
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+  this.hash[key] = key
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+  delete this.hash[key]
+};
+
+/**
+ * Returns true if this set contains the specified element 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+  // key 为0也要返回true
+  return this.hash[key] !== undefined
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
