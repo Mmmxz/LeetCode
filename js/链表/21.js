2e3f29f53a1bbc6a39bb1ca8e2e9b785cab84d45
Mxz <954380531@qq.com>
2021-03-20 16:17:49 +0800

    add 21

---
+++ js/链表/21.js
@@ -0,0 +1,84 @@
+// 21. 合并两个有序链表
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+ 
+
+// 示例 1：
+
+
+// 输入：l1 = [1,2,4], l2 = [1,3,4]
+// 输出：[1,1,2,3,4,4]
+// 示例 2：
+
+// 输入：l1 = [], l2 = []
+// 输出：[]
+// 示例 3：
+
+// 输入：l1 = [], l2 = [0]
+// 输出：[0]
+ 
+
+// 提示：
+
+// 两个链表的节点数目范围是 [0, 50]
+// -100 <= Node.val <= 100
+// l1 和 l2 均按 非递减顺序 排列
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+// 递归
+ var mergeTwoLists = function(l1, l2) {
+  if (!l1) {
+    return l2
+  }
+  if (!l2) {
+    return l1
+  }
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2)
+    return l1
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next)
+    return l2
+  }
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+// 迭代
+ var mergeTwoLists = function(l1, l2) {
+  let dummy = new ListNode(0, null)
+  let cur = dummy
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      cur.next = l1
+      l1 = l1.next
+    } else {
+      cur.next = l2
+      l2 = l2.next
+    }
+    cur = cur.next
+  }
+  cur.next = l1 ? l1 : l2
+  return dummy.next
+};
