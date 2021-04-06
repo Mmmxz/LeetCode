c8a0b2a4ab48f588a6b0368aa7bff59ab3439ef3
Mxz <954380531@qq.com>
2021-04-06 17:07:10 +0800

    add 26 && 80

---
+++ js/双指针/80.js
@@ -0,0 +1,77 @@
+// 80. 删除有序数组中的重复项 II
+// 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 最多出现两次 ，返回删除后数组的新长度。
+
+// 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+ 
+
+// 说明：
+
+// 为什么返回数值是整数，但输出的答案是数组呢？
+
+// 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+
+// 你可以想象内部操作如下:
+
+// // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+// int len = removeDuplicates(nums);
+
+// // 在函数里修改输入数组对于调用者是可见的。
+// // 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
+// for (int i = 0; i < len; i++) {
+//     print(nums[i]);
+// }
+ 
+
+// 示例 1：
+
+// 输入：nums = [1,1,1,2,2,3]
+// 输出：5, nums = [1,1,2,2,3]
+// 解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。 不需要考虑数组中超出新长度后面的元素。
+// 示例 2：
+
+// 输入：nums = [0,0,1,1,1,1,2,3,3]
+// 输出：7, nums = [0,0,1,1,2,3,3]
+// 解释：函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。 不需要考虑数组中超出新长度后面的元素。
+ 
+
+// 提示：
+
+// 1 <= nums.length <= 3 * 104
+// -104 <= nums[i] <= 104
+// nums 已按升序排列
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 解法 1 ：快慢指针
+ var removeDuplicates = function(nums) {
+  const n = nums.length
+  // slow 当前要放元素的位置 fast 依次循环数组
+  let slow = 0, fast = 0
+  while (fast < n) {
+    // fast位置和前两个元素不同 说明应该往slow赋值了
+    if (slow < 2 || nums[fast] !== nums[slow - 2]) {
+      nums[slow] = nums[fast]
+      slow++
+    }
+    fast++
+  }
+  // 最后slow仍然是要放元素的位置 但是已经没有新元素放置 直接返回slow之前的数组即可
+  return slow
+};
+
+// 根据 #26 #80 可以总结通项公式：删除有序数组中的重复项，最多允许 k 个重复
+function process(nums, k) {
+  const n = nums.length
+  let slow = 0, fast = 0
+  while (fast < n) {
+    if (slow < k || nums[fast] !== nums[slow - k]) {
+      nums[slow] = nums[fast]
+      slow++
+    }
+    fast++
+  }
+  return slow
+}
