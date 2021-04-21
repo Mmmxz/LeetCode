5720fb812eeec43497445c52683d449134bf0a16
Mxz <954380531@qq.com>
2021-04-21 17:08:57 +0800

    add 167 && 633

---
+++ js/双指针/633.js
@@ -0,0 +1,50 @@
+// 633. 平方数之和
+// 给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。
+
+ 
+
+// 示例 1：
+
+// 输入：c = 5
+// 输出：true
+// 解释：1 * 1 + 2 * 2 = 5
+// 示例 2：
+
+// 输入：c = 3
+// 输出：false
+// 示例 3：
+
+// 输入：c = 4
+// 输出：true
+// 示例 4：
+
+// 输入：c = 2
+// 输出：true
+// 示例 5：
+
+// 输入：c = 1
+// 输出：true
+ 
+
+// 提示：
+
+// 0 <= c <= 231 - 1
+
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+ var judgeSquareSum = function(c) {
+  let left = 0, right = Math.floor(Math.sqrt(c))
+  while (left <= right) {
+    const sum = Math.pow(left, 2) + Math.pow(right, 2)
+    if (sum === c) {
+      return true
+    } else if (sum > c) {
+      right--
+    } else {
+      left++
+    }
+  }
+  return false
+};
