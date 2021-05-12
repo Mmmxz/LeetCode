a519b00aea18af8cf6819993e353344a244d5ff9
mxz <954380531@qq.com>
2021-05-12 20:22:33 +0800

    add 744

---
+++ js/查找/744.js
@@ -0,0 +1,93 @@
+// 744. 寻找比目标字母大的最小字母
+// 给你一个排序后的字符列表 letters ，列表中只包含小写英文字母。另给出一个目标字母 target，请你寻找在这一有序列表里比目标字母大的最小字母。
+
+// 在比较时，字母是依序循环出现的。举个例子：
+
+// 如果目标字母 target = 'z' 并且字符列表为 letters = ['a', 'b']，则答案返回 'a'
+ 
+
+// 示例：
+
+// 输入:
+// letters = ["c", "f", "j"]
+// target = "a"
+// 输出: "c"
+
+// 输入:
+// letters = ["c", "f", "j"]
+// target = "c"
+// 输出: "f"
+
+// 输入:
+// letters = ["c", "f", "j"]
+// target = "d"
+// 输出: "f"
+
+// 输入:
+// letters = ["c", "f", "j"]
+// target = "g"
+// 输出: "j"
+
+// 输入:
+// letters = ["c", "f", "j"]
+// target = "j"
+// 输出: "c"
+
+// 输入:
+// letters = ["c", "f", "j"]
+// target = "k"
+// 输出: "c"
+ 
+
+// 提示：
+
+// letters长度范围在[2, 10000]区间内。
+// letters 仅由小写字母组成，最少包含两个不同的字母。
+// 目标字母target 是一个小写字母。
+
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+// 二分
+ var nextGreatestLetter = function(letters, target) {
+  const n = letters.length
+  if (target >= letters[n - 1]) {
+    return letters[0]
+  }
+  let left = 0, right = n - 1
+  while (left < right) {
+    const mid = Math.floor((right - left) / 2) + left
+    if (letters[mid] > target) {
+      // 有可能是答案 所以是mid
+      right = mid
+    } else {
+      // <=target 不可能是答案 所以是mid+1
+      left = mid + 1
+    }
+  }
+  return letters[right]
+};
+
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+// 二分 取模运算
+ var nextGreatestLetter = function(letters, target) {
+  const n = letters.length
+  let left = 0, right = n
+  while (left < right) {
+    const mid = Math.floor((right - left) / 2) + left
+    if (letters[mid] > target) {
+      right = mid
+    } else {
+      // <=target
+      left = mid + 1
+    }
+  }
+  // 如果大于所有值 则left和right都会是n，取模即可
+  return letters[left % n]
+};
