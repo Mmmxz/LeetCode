aa10dbf011b091f7371f5db187a22abd2ba3a8e7
mxz <954380531@qq.com>
2021-06-06 14:51:13 +0800

    add 203's answer

--- js/链表/203.js
+++ js/链表/203.js
@@ -31,6 +31,7 @@
  * }
  */
 /**
+ * @description 迭代
  * @param {ListNode} head
  * @param {number} val
  * @return {ListNode}
@@ -46,4 +47,29 @@
     }
   }
   return dummyNode.next
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @description 递归
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+ var removeElements = function(head, val) {
+  if (head === null) {
+    return head
+  }
+  head.next = removeElements(head.next, val)
+  if (head.val === val) {
+    return head.next
+  } else {
+    return head
+  }
 };
