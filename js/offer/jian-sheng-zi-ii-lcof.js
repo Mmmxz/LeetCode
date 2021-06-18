61c6ea6a901a8002cffc33c2e57ba4fa3331bda9
mxz <954380531@qq.com>
2021-06-18 19:51:52 +0800

    add jian-sheng-zi-ii-lcof

---
+++ js/offer/jian-sheng-zi-ii-lcof.js
@@ -0,0 +1,41 @@
+// 剑指 Offer 14- II. 剪绳子 II
+// 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m - 1] 。请问 k[0]*k[1]*...*k[m - 1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+
+// 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+ 
+
+// 示例 1：
+
+// 输入: 2
+// 输出: 1
+// 解释: 2 = 1 + 1, 1 × 1 = 1
+// 示例 2:
+
+// 输入: 10
+// 输出: 36
+// 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+ 
+
+// 提示：
+
+// 2 <= n <= 1000
+// 注意：本题与主站 343 题相同：https://leetcode-cn.com/problems/integer-break/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var cuttingRope = function(n) {
+  // 借用 bigInt 完成大数运算
+  const dp = new Array(n + 1).fill(BigInt(1)), MOD = BigInt(Math.pow(10, 9) + 7)
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j < i; j++) {
+      dp[i] = max(dp[i], BigInt(j) * max(BigInt(i - j), dp[i - j]))
+    }
+  }
+  return dp[n] % MOD
+};
+const max = (...args) => {
+  return args.reduce((prev, cur) => prev > cur ? prev : cur)
+}
