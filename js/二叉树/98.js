465e37d3d31648f80e9feebb8e574ae9e36a619f
mxz <954380531@qq.com>
2021-11-30 16:47:14 +0800

    add 98

---
+++ js/二叉树/98.js
@@ -0,0 +1,51 @@
+// 98. 验证二叉搜索树
+// 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+
+// 有效 二叉搜索树定义如下：
+
+// 节点的左子树只包含 小于 当前节点的数。
+// 节点的右子树只包含 大于 当前节点的数。
+// 所有左子树和右子树自身必须也是二叉搜索树。
+ 
+
+// 示例 1：
+
+
+// 输入：root = [2,1,3]
+// 输出：true
+// 示例 2：
+
+
+// 输入：root = [5,1,4,null,null,3,6]
+// 输出：false
+// 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ 
+
+// 提示：
+
+// 树中节点数目范围在[1, 104] 内
+// -231 <= Node.val <= 231 - 1
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  return helper(root, -Infinity, Infinity)
+};
+
+const helper = (root, low, high) => {
+  if (!root) return true
+  if (root.val <= low || root.val >= high) {
+    return false
+  }
+  return helper(root.left, low, root.val) && helper(root.right, root.val, high)
+}
