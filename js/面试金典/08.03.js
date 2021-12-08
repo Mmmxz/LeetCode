79abaaef71111d3f5c76c8a1b37299d890b20ac0
mxz <954380531@qq.com>
2021-12-08 19:51:43 +0800

    add 08.03

---
+++ js/面试金典/08.03.js
@@ -0,0 +1,35 @@
+// 面试题 08.03. 魔术索引
+// 魔术索引。 在数组A[0...n-1]中，有所谓的魔术索引，满足条件A[i] = i。给定一个有序整数数组，编写一种方法找出魔术索引，若有的话，在数组A中找出一个魔术索引，如果没有，则返回-1。若有多个魔术索引，返回索引值最小的一个。
+
+// 示例1:
+
+//  输入：nums = [0, 2, 3, 4, 5]
+//  输出：0
+//  说明: 0下标的元素为0
+// 示例2:
+
+//  输入：nums = [1, 1, 1]
+//  输出：1
+// 说明:
+
+// nums长度在[1, 1000000]之间
+// 此题为原书中的 Follow-up，即数组中可能包含重复元素的版本
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMagicIndex = function(nums) {
+  let res = Infinity
+  const find = (lo, hi) => {
+    if (lo > hi) return
+    const mid = (lo + hi) >>> 1
+    if (nums[mid] == mid) {
+      res = Math.min(res, mid)
+    }
+    find(lo, mid - 1)
+    find(mid + 1, hi)
+  }
+  find(0, nums.length - 1)
+  return res === Infinity ? -1 : res
+};
