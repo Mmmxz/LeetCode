3095a30818438d35b6c8b21ce589fdc3aac795b1
mxz <954380531@qq.com>
2022-01-31 11:01:00 +0800

    add 02.01

---
+++ js/面试金典/02.01.js
@@ -0,0 +1,46 @@
+// 面试题 02.01. 移除重复节点
+// 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+
+// 示例1:
+
+//  输入：[1, 2, 3, 3, 2, 1]
+//  输出：[1, 2, 3]
+// 示例2:
+
+//  输入：[1, 1, 1, 1, 2]
+//  输出：[1, 2]
+// 提示：
+
+// 链表长度在[0, 20000]范围内。
+// 链表元素在[0, 20000]范围内。
+// 进阶：
+
+// 如果不得使用临时缓冲区，该怎么解决？
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeDuplicateNodes = function(head) {
+  let cur = head
+  let memo = new Set()
+  while (cur && cur.next) {
+    memo.add(cur.val)
+    const next = cur.next
+    if (memo.has(next.val)) {
+      // 删除next
+      cur.next = next.next
+    } else {
+      // 只有不删除时 才能正常指向下一个
+      cur = cur.next
+    }
+  }
+  return head
+};
